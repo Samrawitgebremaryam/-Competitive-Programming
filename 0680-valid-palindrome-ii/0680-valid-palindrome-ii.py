2e4fbf6b2a00b802e71107f6b8b020ca7834123d
Samrawit Gebremaryam Bahta <130246151+Samrawitgebremaryam@users.noreply.github.com>
2023-11-08 19:59:32 +0300

    Time: 96 ms (72.84%), Space: 16.8 MB (47.96%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,15 +1,16 @@
 class Solution(object):
     def validPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        # Time: O(n)
-        # Space: O(n)
-        left, right = 0, len(s) - 1
-        while left < right:
-            if s[left] != s[right]:
-                one, two = s[left:right], s[left + 1:right + 1]
-                return one == one[::-1] or two == two[::-1]
-            left, right = left + 1, right - 1
+        l,r =0, len(s)-1
+        while l<r:
+            if s[l]==s[r]:
+                l,r=l+1,r-1
+            else:
+                one=s[:l]+s[l+1:]
+                two=s[:r]+s[r+1:]
+                if one==one[::-1] or two==two[::-1]:
+                    return True 
+                else :
+                    return False
         return True
+
+
