fb121129ae89314b8c1e77002910a745ae64c507
Samrawit Gebremaryam Bahta <130246151+Samrawitgebremaryam@users.noreply.github.com>
2024-02-26 10:18:20 +0300

    Add solution for Fibonacci Number

--- Progress-sheet/leetcode/fibonacci-number.py
+++ Progress-sheet/leetcode/fibonacci-number.py
@@ -1,8 +1,23 @@
 class Solution:
     def fib(self, n: int) -> int:
         if n<=1:
-            ans=n
-        else:
-            ans = self.fib(n-1)+self.fib(n-2)
-        return ans 
+            return n
+        return self.fib(n-1)+self.fib(n-2)
+
+
+
+
+
+
+
+
+
+
+
+
+        # if n<=1:
+        #     ans=n
+        # else:
+        #     ans = self.fib(n-1)+self.fib(n-2)
+        # return ans 
         
