28d7abe3f6a1bac2b8adcd58b04db1e02fbd0e23
Samrawit Gebremaryam Bahta <130246151+Samrawitgebremaryam@users.noreply.github.com>
2024-02-29 12:03:11 +0300

    Add solution for Validate Binary Search Tree

---
+++ Progress-sheet/leetcode/validate-binary-search-tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        ans=[]
+        def inorder (root):
+            if root:
+                inorder(root.left)
+                ans.append(root.val)
+                inorder(root.right)
+        inorder(root)
+        #  if it is a strictly increasing when we do inorder 
+        # traversal it is binary search tree 
+        for i in range (len(ans)-1):
+            if ans[i]>=ans[i+1]:
+                return False
+        else:
+            return True  
+        
+
